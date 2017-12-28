92668c5759a15bd2ff74fb28864602b3a2f2cdd9
shahzeb.ihsan <shahzeb.ihsan@continental-corporation.com>
2017-12-28 14:58:18 +0800

    First commit...kinda works!

---
+++ lua_interface/LuaInterface.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedLua
+{
+    public class Lua
+    {
+        public const int LUA_MULTRET = (-1);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        public delegate void Callback (IntPtr lua_State);
+
+        [DllImport("lua_core.dll")]
+        public static extern IntPtr luaL_newstate();
+
+        [DllImport("lua_core.dll")]
+        public static extern void lua_pushcclosure(IntPtr luaState, [MarshalAs(UnmanagedType.FunctionPtr)] Callback func, int n);
+
+        [DllImport("lua_core.dll")]
+        public static extern void lua_setglobal(IntPtr luaState, string name);
+
+        //open all Lua libraries
+        [DllImport("lua_core.dll")]
+        public static extern void luaL_openlibs(IntPtr luaState);
+
+        [DllImport("lua_core.dll")]
+        public static extern void lua_close(IntPtr luaState);
+
+        [DllImport("lua_core.dll")]
+        public static extern int luaL_loadstring(IntPtr luaState, string s);
+
+        [DllImport("lua_core.dll")]
+        public static extern int lua_getglobal(IntPtr luaState, string name);
+
+        [DllImport("lua_core.dll")]
+        public static extern int lua_tointegerx(IntPtr luaState, int ignore0, int ignore1);
+
+        [DllImport("lua_core.dll")]
+        public static extern int lua_pcallk(IntPtr luaState, int nargs, int nresults, int errfunc, int ignore0, int ignore1);
+
+        public static int LoadString(IntPtr luaState, string s)
+        {
+            if (luaL_loadstring(luaState, s) != 0)
+                return 1;
+            return lua_pcallk(luaState, 0, LUA_MULTRET, 0, 0, 0);
+        }
+
+        public static void ExposeFunction(IntPtr luaState, Callback func, string name)
+        {
+            Lua.lua_pushcclosure(luaState, func, 0);
+            Lua.lua_setglobal(luaState, name);
+        }
+
+        public static void CallFunction(IntPtr luaState, string name)
+        {
+            Lua.lua_getglobal(luaState, name);
+            Lua.lua_pcallk(luaState, 0, 1, 0, 0, 0);
+        }
+
+        public static int GetIntegerReturn(IntPtr luaState)
+        {
+            return Lua.lua_tointegerx(luaState, -1, 0);
+        }
+
+        /*
+        public static void LogError()
+        {
+           //log error
+           string errMsg = null;
+           if (Lua.lua_isstring(m_lua_State, -1) &gt; 0)
+               errMsg = Lua.lua_tostring(m_lua_State, -1);
+
+           //clear the Lua stack
+           Lua.lua_settop(m_lua_State, 0);
+
+           //log or show the error somewhere in your program
+        }
+        */
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JaggedLua;
+using System.Threading;
+
+namespace test
+{
+    class Program
+    {
+        static string luaScript =
+            @"
+            print('Lua: Hello World, I am', _VERSION);
+            print('Lua: ...and now I will call a C# function!');
+            sharpFunc0();
+
+            function luaFunc0()
+                r = 0xDEADBEEF;
+                print('Lua: luaFunc0 - I will return ', string.format('0x%X', r));
+                return r;
+            end;
+
+            function luaFunc1()
+                tick = 0
+                while true do
+                    io.write('\rLua: luaFunc1 - tick = ', tick);
+                    tick = tick + 1;
+                    sharpSleep();
+                end;
+            end;";
+
+        static void luaThread()
+        {
+            IntPtr luaState = Lua.luaL_newstate();
+
+            Lua.Callback sharpFunc0 =
+                (IntPtr _luaState) =>
+                {
+                    Console.WriteLine("C#: sharpFunc0: ...");
+                };
+
+            Lua.Callback sharpSleep =
+                (IntPtr _luaState) =>
+                {
+                    Thread.Sleep(1000);
+                };
+
+            if (luaState != IntPtr.Zero)
+            {
+
+                Lua.luaL_openlibs(luaState);
+
+                Lua.ExposeFunction(luaState, sharpFunc0, "sharpFunc0");
+                Lua.ExposeFunction(luaState, sharpSleep, "sharpSleep");
+
+                //string luaScriptString = "test(); TwoPlusTwo = 2+2; print('Hello World'); print('TwoPlusTwo:', TwoPlusTwo);function MyFunction ()\n  print('MyFunction');while true do   print('Hello');  sleep();  end;return 65;\nend;return 5";
+                if (Lua.LoadString(luaState, luaScript) != 0)
+                {
+                    Lua.lua_close(luaState);
+                }
+
+                Lua.CallFunction(luaState, "luaFunc0");
+                Console.WriteLine("C#: luaFunc0 returned 0x{0:X}", Lua.GetIntegerReturn(luaState));
+
+                Lua.CallFunction(luaState, "luaFunc1");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var t = Task.Run(() => luaThread());
+
+            t.Wait();
+            Console.WriteLine("Waiting...");
+        }
+    }
+}
