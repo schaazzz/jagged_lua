39a234cee9f039cf2b2436fe1eb42e928aef63ff
shahzeb.ihsan <shahzeb.ihsan@continental-corporation.com>
2018-01-03 23:32:26 +0800

    Added interfaces for passing and returning arguments from C# functions. Expanded Lua script example to demonstrate how to test all available functionality

--- lua_interface/LuaInterface.cs
+++ lua_interface/LuaInterface.cs
@@ -12,7 +12,7 @@ namespace JaggedLua
         public const int LUA_MULTRET = (-1);
         
         [UnmanagedFunctionPointer(CallingConvention.StdCall)]
-        public delegate void Callback (IntPtr lua_State);
+        public delegate int Callback (IntPtr lua_State);
 
         [DllImport("lua_core.dll")]
         private static extern IntPtr luaL_newstate();
@@ -33,6 +33,9 @@ namespace JaggedLua
         private static extern int luaL_loadstring(IntPtr luaState, string s);
 
         [DllImport("lua_core.dll")]
+        private static extern void lua_pushinteger(IntPtr luaState, UInt64 n);
+
+        [DllImport("lua_core.dll")]
         private static extern int lua_getglobal(IntPtr luaState, string name);
 
         [DllImport("lua_core.dll")]
@@ -40,6 +43,32 @@ namespace JaggedLua
 
         [DllImport("lua_core.dll")]
         private static extern int lua_pcallk(IntPtr luaState, int nargs, int nresults, int errfunc, int ignore0, int ignore1);
+
+        [DllImport("lua_core.dll")]
+        private static extern int lua_gettop(IntPtr luaState);
+
+        [DllImport("lua_core.dll")]
+        private static extern int luaL_checkinteger(IntPtr luaState, int arg);
+
+        [DllImport("lua_core.dll")]
+        private static extern IntPtr luaL_checklstring(IntPtr luaState, int arg, ref int length);
+
+        private static string PtrToStringUtf8(IntPtr ptr)
+        {
+            if (ptr == IntPtr.Zero)
+                return "";
+
+            int length = 0;
+            while (Marshal.ReadByte(ptr, length) != 0)
+                length++;
+
+            if (length == 0)
+                return "";
+
+            byte[] array = new byte[length];
+            Marshal.Copy(ptr, array, 0, length);
+            return System.Text.Encoding.UTF8.GetString(array);
+        }
 
         public static IntPtr NewState()
         {
@@ -82,6 +111,45 @@ namespace JaggedLua
         public static int GetIntegerReturn(IntPtr luaState)
         {
             return lua_tointegerx(luaState, -1, 0);
+        }
+
+        public static void ReturnInteger(IntPtr luaState, UInt64 value)
+        {
+            lua_pushinteger(luaState, value);
+        }
+
+        public static int NumArgs(IntPtr luaState)
+        {
+            return lua_gettop(luaState);
+        }
+
+        public static Tuple<bool, int> GetIntegerArg(IntPtr luaState, int index)
+        {
+            int retVal = 0;
+            bool success = false;
+
+            if (lua_gettop(luaState) >= 1)
+            {
+                retVal = luaL_checkinteger(luaState, index);
+                success = true;
+            }
+
+            return Tuple.Create(success, retVal);
+        }
+
+        public static Tuple<bool, int, string> GetStringArg(IntPtr luaState, int index)
+        {
+            string retVal = "";
+            int length = 0;
+            bool success = false;
+
+            if (lua_gettop(luaState) >= 1)
+            {
+                retVal = PtrToStringUtf8(luaL_checklstring(luaState, index, ref length));
+                success = true;
+            }
+
+            return Tuple.Create(success, length, retVal);
         }
 
         public static void Close(IntPtr luaState)
--- test/Program.cs
+++ test/Program.cs
@@ -12,44 +12,96 @@ namespace test
     {
         static string luaScript =
             @"
-            print('Lua: Hello World, I am', _VERSION);
-            print('Lua: ...and now I will call a C# function!');
-            sharpFunc0();
+            print('Lua: Hello World, I am', _VERSION)
+            
+            print('Lua: ...let\'s call a C# function without any parameters!')
+            sharpFunc0()
+            
+            print('Lua: ...now let\'s try to call a C# function with an integer and a string parameter!')
+            sharpFunc1(0xFACADE, 'Hello C#')
+
+            print('Lua: ...now we\'ll call a C# function which returns a value!')            
+            n = sharpFunc2()
+            print('Lua: sharpFunc2 returned', string.format('0x%X', n))
+
+            io.write('Lua: Did it work? [y/n] ')
+            
+            i = ''
+            while true do
+                i = io.read()
+                if i:upper() == 'Y' or i:upper() == 'N' then
+                    break
+                else
+                    io.write('Lua: I\'m afraid I can only accept \'y\' or \'n\' as input...')
+                end
+            end
 
             function luaFunc0()
-                r = 0xDEADBEEF;
-                print('Lua: luaFunc0 - I will return ', string.format('0x%X', r));
-                return r;
-            end;
+                r = 0xDEADBEEF
+                print('Lua: luaFunc0 - I will return', string.format('0x%X', r))
+                return r
+            end
 
             function luaFunc1()
                 tick = 0
                 while true do
-                    io.write('\rLua: luaFunc1 - tick = ', tick);
-                    tick = tick + 1;
-                    sharpSleep();
-                end;
-            end;";
+                    io.write('\rLua: luaFunc1 - tick = ', tick)
+                    tick = tick + 1
+                    sharpSleep(1000)
+                end
+            end";
 
         static void luaThread()
         {
             IntPtr luaState = Lua.NewState();
 
-            Lua.Callback sharpFunc0 =
-                (IntPtr _luaState) =>
+            Lua.Callback sharpFunc0 = (IntPtr _luaState) =>
+            {
+                Console.WriteLine("C#: sharpFunc0");
+                return 0;
+            };
+
+            Lua.Callback sharpFunc1 = (IntPtr _luaState) =>
+            {
+                Tuple<bool, int> arg0 = Lua.GetIntegerArg(luaState, 1);
+                Tuple<bool, int, string> arg1 = Lua.GetStringArg(_luaState, 2);
+
+                if (arg0.Item1 && arg1.Item1 && (arg1.Item2 != 0))
                 {
-                    Console.WriteLine("C#: sharpFunc0: ...");
-                };
+                    Console.WriteLine("C#: sharpFunc1 - Args [Integer: 0x{0:X}, String (length {1}): '{2}']", arg0.Item2, arg1.Item2, arg1.Item3);
+                }
+
+                return 0;
+            };
+
+            Lua.Callback sharpFunc2 = (IntPtr _luaState) =>
+            {
+                UInt64 retVal = 0xBAADF00D;
+                Console.WriteLine("C#: sharpFunc2 - I will return 0x{0:X}", retVal);
+
+                Lua.ReturnInteger(_luaState, retVal);
+                return 1;
+            };
 
             Lua.Callback sharpSleep =
                 (IntPtr _luaState) =>
                 {
-                    Thread.Sleep(1000);
+                    int delay = 100;
+                    if (Lua.NumArgs(_luaState) == 1)
+                    {
+                        Tuple<bool, int> arg = Lua.GetIntegerArg(luaState, 1);
+                        delay = arg.Item1 ? arg.Item2 : delay;
+
+                    }
+                    Thread.Sleep(delay);
+                    return 0;
                 };
 
             if (luaState != IntPtr.Zero)
             {
                 Lua.ExposeFunction(luaState, sharpFunc0, "sharpFunc0");
+                Lua.ExposeFunction(luaState, sharpFunc1, "sharpFunc1");
+                Lua.ExposeFunction(luaState, sharpFunc2, "sharpFunc2");
                 Lua.ExposeFunction(luaState, sharpSleep, "sharpSleep");
 
                 if (Lua.LoadString(luaState, luaScript) != 0)
